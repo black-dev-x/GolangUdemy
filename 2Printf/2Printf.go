b771be74d6628b4e01fd31fe75a31bb5fe406132
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-11-17 23:46:33 -0300

    Update printf statements in 2Printf.go

--- 2Printf/2Printf.go
+++ 2Printf/2Printf.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	const name, age = "Kim", 22
-	fmt.Printf("%s is %d years old.\n", name, age)
+	// two ways of doing the same thing
+	// %s is for string, %d is for number
+	// %v is for value and %T is for type
+	fmt.Printf("name: %s age: %d.\n", name, age)
+	fmt.Printf("name: %v age: %v.\n", name, age)
+	fmt.Printf("name type: %T age type: %T.\n", name, age)
 }
