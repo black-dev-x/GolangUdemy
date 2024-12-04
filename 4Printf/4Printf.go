5d916e6e91cafd65da0cbd27e861b45830dee5fa
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-03 21:58:16 -0300

    Update Printf format specifiers

--- 4Printf/4Printf.go
+++ 4Printf/4Printf.go
@@ -11,10 +11,10 @@ func main() {
 
 	// print these values as both binary & hexadecimal
 	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
-	fmt.Printf("a:%v \t %b \t %X \n", a, a, a)
-	fmt.Printf("b:%v \t %b \t %X \n", b, b, b)
-	fmt.Printf("c:%v \t %b \t %X \n", c, c, c)
-	fmt.Printf("d:%v \t %b \t %X \n", d, d, d)
-	fmt.Printf("e:%v \t %b \t %X \n", e, e, e)
-	fmt.Printf("f:%v \t %b \t %X \n", f, f, f)
+	fmt.Printf("a:%v \t %b \t %#x \n", a, a, a)
+	fmt.Printf("b:%v \t %b \t %#x \n", b, b, b)
+	fmt.Printf("c:%v \t %b \t %#x \n", c, c, c)
+	fmt.Printf("d:%v \t %b \t %#x \n", d, d, d)
+	fmt.Printf("e:%v \t %b \t %#x \n", e, e, e)
+	fmt.Printf("f:%v \t %b \t %#x \n", f, f, f)
 }
